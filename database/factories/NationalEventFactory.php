67ae114e633b461d3a8603817851295a7ba353b6
uusa35 <uusa35@gmail.com>
2022-11-25 08:22:27 +0300

    new packages

--- database/factories/NationalEventFactory.php
+++ database/factories/NationalEventFactory.php
@@ -8,11 +8,10 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
-// * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Nationalevent>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Nationalevent>
  */
 class NationalEventFactory extends Factory
 {
-    protected $model = Nationalevent::class;
     /**
      * Define the model's default state.
      *
